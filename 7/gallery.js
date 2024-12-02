c0557252b9296553b93a24d74ac3de00d8b1570b
UlyanaLukyanenko <lukyanenko.ulyana@gmail.com>
2024-12-03 00:23:43 +0300

    Add files via upload

---
+++ 7/gallery.js
@@ -0,0 +1,16 @@
+
+$(document).ready(function () {
+    $(".carousel").slick({
+        dots: true,
+        infinite: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+        ],
+        slidesToShow: 3
+    });
+});
